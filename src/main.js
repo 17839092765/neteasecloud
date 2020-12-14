375566b40185e431d440ac097aab009721766f8d
MuXi <1341307180@qq.com>
2020-12-14 12:18:59 +0800

    111

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-<<<<<<< HEAD
 
 import Vant from "vant";
 import "vant/lib/index.css";
 
 Vue.use(Vant);
-=======
+
 import request from "./util/request";
 Vue.prototype.$request = request;
->>>>>>> dev
+
 Vue.config.productionTip = false;
 
 new Vue({
