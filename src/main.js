d65292823b1be6840384bfe5d4b8b352b8938a3a
wangxb-jpg <3453359121@qq.com>
2020-12-15 13:42:51 +0800

    王希博第一次提交

--- src/main.js
+++ src/main.js
@@ -2,15 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import rem from "./util/rem";
+Vue.use(rem);
+
 import Vant from "vant";
 import "vant/lib/index.css";
-
 Vue.use(Vant);
-Vue.use(rem);
-import { Icon } from "vant";
 
+import { Icon } from "vant";
 Vue.use(Icon);
+
+import { Toast } from 'vant';
+Vue.use(Toast);
+
 import request from "./util/request";
 Vue.prototype.$request = request;
 
