069a910ac90e00d669b33856cc060a8fbd0f6129
loulou <593711751@qq.com>
2020-12-15 20:55:31 +0800

    123

--- src/views/podcast/children/recommend.vue
+++ src/views/podcast/children/recommend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="recommend">
     <div class="boke">
       <span>
         <van-icon
@@ -59,6 +59,64 @@
         </figure>
       </div>
     </div>
+    <div class="bodan">
+      <div class="bodan-top">
+        <p>音乐故事|这有一个故事陪你听歌</p>
+        <van-tag plain type="primary">更多></van-tag>
+      </div>
+      <div class="bodan-bottom">
+        <figure>
+          <img
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608028894775&di=62ecf81776ddcdf0fef9aed3bf7c7b91&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F18%2F07%2F10%2F3e4575f9df9afb8c4d49883b2f5cfce6.jpg"
+            alt=""
+          />
+          <figcaption>你一定会爱上的惊艳男生翻唱</figcaption>
+        </figure>
+        <figure>
+          <img
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608028894775&di=62ecf81776ddcdf0fef9aed3bf7c7b91&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F18%2F07%2F10%2F3e4575f9df9afb8c4d49883b2f5cfce6.jpg"
+            alt=""
+          />
+          <figcaption>你一定会爱上的惊艳男生翻唱</figcaption>
+        </figure>
+        <figure>
+          <img
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608028894775&di=62ecf81776ddcdf0fef9aed3bf7c7b91&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F18%2F07%2F10%2F3e4575f9df9afb8c4d49883b2f5cfce6.jpg"
+            alt=""
+          />
+          <figcaption>你一定会爱上的惊艳男生翻唱</figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="bodan">
+      <div class="bodan-top">
+        <p>音乐故事|这有一个故事陪你听歌</p>
+        <van-tag plain type="primary">更多></van-tag>
+      </div>
+      <div class="bodan-bottom">
+        <figure>
+          <img
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608028894775&di=62ecf81776ddcdf0fef9aed3bf7c7b91&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F18%2F07%2F10%2F3e4575f9df9afb8c4d49883b2f5cfce6.jpg"
+            alt=""
+          />
+          <figcaption>你一定会爱上的惊艳男生翻唱</figcaption>
+        </figure>
+        <figure>
+          <img
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608028894775&di=62ecf81776ddcdf0fef9aed3bf7c7b91&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F18%2F07%2F10%2F3e4575f9df9afb8c4d49883b2f5cfce6.jpg"
+            alt=""
+          />
+          <figcaption>你一定会爱上的惊艳男生翻唱</figcaption>
+        </figure>
+        <figure>
+          <img
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608028894775&di=62ecf81776ddcdf0fef9aed3bf7c7b91&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F18%2F07%2F10%2F3e4575f9df9afb8c4d49883b2f5cfce6.jpg"
+            alt=""
+          />
+          <figcaption>你一定会爱上的惊艳男生翻唱</figcaption>
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -84,50 +142,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.boke {
-  padding: 10px;
-  border: 1px solid red;
-  border-radius: 20px;
-  margin: 20px 10px;
-  display: flex;
-  justify-content: space-between;
-  // background: pink;
-  span {
-    font-size: 16px;
-    vertical-align: middle;
-    line-height: 20px;
-    i {
-      vertical-align: middle;
-    }
-  }
-}
-.bodan {
-  .bodan-top {
+.recommend {
+  padding-bottom: 50px;
+  .boke {
+    padding: 0.2rem;
+    border: 1px solid red;
+    border-radius: 0.4rem;
+    margin: 0.5rem 0.2rem;
     display: flex;
     justify-content: space-between;
-    p {
-      font-size: 16px;
-      font-weight: 600;
-    }
+    // background: pink;
     span {
-      font-size: 12px;
-      padding: 3px 6px;
-      border-radius: 10px;
+      font-size: 0.32rem;
+      vertical-align: middle;
+      line-height: 0.4rem;
+      i {
+        vertical-align: middle;
+      }
     }
   }
-  .bodan-bottom {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    figure {
-      margin: 10px 0;
-      width: 30%;
-      background: pink;
+  .bodan {
+    .bodan-top {
+      display: flex;
+      justify-content: space-between;
+      p {
+        font-size: 0.32rem;
+        font-weight: 600;
+      }
+      span {
+        font-size: 0.24rem;
+        padding: 0.06rem 0.12rem;
+        border-radius: 10px;
+      }
     }
-  }
-  img {
-    width: 50px;
+    .bodan-bottom {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      figure {
+        margin: 10px 0;
+        width: 30%;
+        background: pink;
+        figcaption {
+          margin: 0.2rem 0;
+        }
+      }
+    }
+    img {
+      width: 100px;
+    }
   }
 }
 </style>
