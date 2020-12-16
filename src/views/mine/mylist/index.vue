3f422a1340b5eca27abe6632ef27d72e900d9ed4
sheliy <2193633969@qq.com>
2020-12-16 10:07:59 +0800

    123

--- src/views/mine/mylist/index.vue
+++ src/views/mine/mylist/index.vue
@@ -1,68 +1,55 @@
 <!-- 组件说明 -->
 <template>
-    <div class='index'>
-        <van-nav-bar
-  title="标题"
-  left-text="返回"
-  right-text="按钮"
-  left-arrow
-  @click-left="onClickLeft"
-  @click-right="onClickRight"
-/>
-        我的账号1
-    </div>
+  <div class="index">
+    <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
+    我的账号1
+  </div>
 </template>
 
 <script>
-import { Toast } from 'vant';
-    export default {
-        components: {
-
-        },
-        data () {
-            return {
-
-            };
-        },
-        computed: {
-
-        },
-        watch: {
-
-        },
-        methods: {
-onClickLeft() {
-      Toast('返回');
+import { Toast } from "vant";
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    onClickLeft() {
+      Toast("返回");
     },
     onClickRight() {
-      Toast('按钮');
+      Toast("按钮");
     },
-        },
-        created() {
-
-},
-        mounted() {
-
-},
-beforeCreate() {}, //生命周期 - 创建之前
-beforeMount() {}, //生命周期 - 挂载之前
-beforeUpdate() {}, //生命周期 - 更新之前
-updated() {}, //生命周期 - 更新之后
-beforeDestroy() {}, //生命周期 - 销毁之前
-destroyed() {}, //生命周期 - 销毁完成
-activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
-    }
+  },
+  created() {},
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
 </script>
 
-<style lang='scss' scoped>
-
-.index{
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    background: rgb(116, 167, 118);
-    z-index: 1;
+<style lang="scss" scoped>
+.index {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: rgb(116, 167, 118);
+  z-index: 1;
 }
 </style>
