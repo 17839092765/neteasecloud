fb3d582784a596a53fbebd27512808f43584aaad
loulou <593711751@qq.com>
2020-12-18 13:52:34 +0800

    09

--- src/views/mine/mine.vue
+++ src/views/mine/mine.vue
@@ -65,10 +65,25 @@
     </el-row>
     <van-tabs v-model="activeName">
       <van-tab title="创建歌单" name="a">
-        <ul v-for="item in musiclist" :key="item.id" class="musiclist">
+        <ul
+          class="musiclist"
+          style="padding:20px 0 0 20px;color:#000;font-weight:900;font-size:16px"
+        >
+          <li style="margin:0 20px 0 0">歌单图片</li>
+          <li style="margin:0 20px">歌单名</li>
+          <li style="margin:0 20px">数量</li>
+        </ul>
+        <ul
+          v-for="item in musiclist"
+          :key="item.id"
+          class="musiclist"
+          style="margin:20px 0"
+        >
           <img :src="item.coverImgUrl" alt="" />
-          <li>{{ item.name }}</li>
-          <li>{{ item.trackCount }}首</li>
+          <li style="height:50px;width:150px;text-align:center;">
+            {{ item.name }}
+          </li>
+          <li style="height:50px">{{ item.trackCount }}首</li>
         </ul>
       </van-tab>
       <van-tab title="收藏歌单" name="b">
@@ -84,231 +99,238 @@
 </template>
 
 <script>
-  import { Toast } from "vant";
-  export default {
-    data() {
-      return {
-        uid: "",
-        activeName: "a",
-        musiclist: [],
-        usermsg: {
-          name: "",
-          userimg: "",
-          level: "",
-        },
-      };
-    },
-    computed: {},
-    watch: {},
-    methods: {
-      getlist() {
-        this.$request.get(`/user/playlist?uid=${this.uid}`).then((res) => {
-          this.musiclist = res.playlist;
-          console.log(res);
-        });
+import { Toast } from "vant";
+export default {
+  data() {
+    return {
+      uid: "",
+      activeName: "a",
+      musiclist: [],
+      usermsg: {
+        name: "",
+        userimg: "",
+        level: "",
       },
-      getuser() {
-        this.$request.post(`user/detail?uid=${this.uid}`).then((res) => {
-          console.log(res);
-          // this.usermsg=res
-          this.usermsg.name = res.profile.nickname;
-          this.usermsg.userimg = res.profile.avatarUrl;
-          this.usermsg.level = res.level;
-        });
-      },
-      person() {
-        console.log(111);
-        this.$router.push("/mine/mylist");
-      },
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    getlist() {
+      this.$request.get(`/user/playlist?uid=${this.uid}`).then((res) => {
+        this.musiclist = res.playlist;
+        console.log(res);
+      });
     },
-    created() {
-      let uid = this.$cookie.get("uid");
-      this.uid = uid;
-      console.log(uid);
-      this.getlist();
-      this.getuser();
+    getuser() {
+      this.$request.post(`user/detail?uid=${this.uid}`).then((res) => {
+        console.log(res);
+        // this.usermsg=res
+        this.usermsg.name = res.profile.nickname;
+        this.usermsg.userimg = res.profile.avatarUrl;
+        this.usermsg.level = res.level;
+      });
     },
-    mounted() {},
-    beforeCreate() {},
-    beforeMount() {},
-    beforeUpdate() {},
-    updated() {},
-    beforeDestroy() {},
-    destroyed() {},
-    activated() {},
-    components: {},
-  };
+    person() {
+      console.log(111);
+      this.$router.push("/mine/mylist");
+    },
+  },
+  created() {
+    let uid = this.$cookie.get("uid");
+    this.uid = uid;
+    console.log(uid);
+    this.getlist();
+    this.getuser();
+  },
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+  components: {},
+};
 </script>
 
 <style lang="scss" scoped>
-  .mine {
-    position: fixed;
-    top: 0;
-    width: 100%;
-    bottom: 2rem;
-    left: 0;
-    overflow: scroll;
-    img {
-      width: 60px;
-    }
-    margin: 0 auto;
-    background-color: #f7f7f7;
-    .header {
-      height: 100px;
-      position: relative;
-      .photo {
-        margin-left: 30px;
-        margin-top: 50px;
-        img {
-          width: 70px;
-          height: 70px;
-          border-radius: 50%;
-          display: block;
-          float: left;
-        }
-        p {
-          font-size: 14px;
-          font-weight: 600;
-          float: left;
-          margin-top: 20px;
-          margin-left: 10px;
-        }
-        span:nth-child(1) {
-          text-align: center;
-          float: left;
-          display: block;
-          font-size: 14px;
-          width: 60px;
-          padding: 0 5px;
-          height: 22px;
-          line-height: 22px;
-          background: #000;
-          color: #fff;
-          border-radius: 10px;
-          position: absolute;
-          left: 110px;
-          bottom: 35px;
-        }
-        span:nth-child(2) {
-          text-align: center;
-          width: 40px;
-          height: 20px;
-          border-radius: 10px;
-          background-color: #fff;
-          position: absolute;
-          left: 180px;
-          bottom: 35px;
-          padding: 0 5px;
-          line-height: 20px;
-        }
-      }
-    }
-    .van-icon {
-      font-size: 20px;
-      position: absolute;
-      top: 30px;
-      right: 30px;
-    }
-
-    .list {
-      height: 160px;
-      width: 90%;
-      background-color: #ffffff;
-      margin: 0 auto;
-      border-radius: 10px;
-      .list-inner {
-        height: 80px;
-        text-align: center;
-        #tubiao {
-          font-size: 36px;
-          margin-top: 10px;
-          color: red;
-        }
-        #tubiao8 {
-          font-size: 36px;
-          margin-top: 10px;
-          color: #a8a8a8;
-        }
-        p {
-          height: 20px;
-          line-height: 20px;
-          margin-top: 5px;
-          color: #5a5a5a;
-        }
-      }
-    }
-    .like {
-      height: 80px;
-      width: 90%;
-      margin: 10px auto;
-      background-color: #fff;
-      padding: 10px 10px;
-      position: relative;
-      border-radius: 10px;
-      .like-left {
-        height: 100%;
-        width: 60px;
-        background-color: #000;
-        opacity: 0.9;
-        img {
-          width: 100%;
-          height: 100%;
-          border-radius: 10px;
-        }
-      }
-      .like-main {
+.mine {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  bottom: 2rem;
+  left: 0;
+  overflow: scroll;
+  img {
+    width: 60px;
+    // margin-left: 10px;
+  }
+  margin: 0 auto;
+  background-color: #f7f7f7;
+  .header {
+    height: 100px;
+    position: relative;
+    .photo {
+      margin-left: 30px;
+      margin-top: 50px;
+      img {
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+        display: block;
         float: left;
-        p:nth-child(1) {
-          margin-left: 15px;
-          margin-top: 5px;
-          font-size: 16px;
-        }
-        p:nth-child(2) {
-          margin-left: 15px;
-          margin-top: 2px;
-          color: #8d8d8d;
-          font-size: 12px;
-        }
       }
-      .like-right {
-        p {
-          width: 60px;
-          height: 13px;
-          border-radius: 14px;
-          padding: 5px 7px;
-          line-height: 13px;
-          border: 1px solid #8d8d8d;
-          position: absolute;
-          top: 27px;
-          right: 20px;
-          i {
-            height: 100%;
-          }
-          span {
-            height: 100%;
-          }
-        }
-      }
-    }
-    .van-tabs {
-      height: 50px;
-      width: 90%;
-      margin: 0 20px;
       p {
-        font-size: 16px;
+        font-size: 14px;
+        font-weight: 600;
+        float: left;
+        margin-top: 20px;
+        margin-left: 10px;
+      }
+      span:nth-child(1) {
         text-align: center;
-        line-height: 45px;
-        color: #000;
+        float: left;
+        display: block;
+        font-size: 14px;
+        width: 60px;
+        padding: 0 5px;
+        height: 22px;
+        line-height: 22px;
+        background: #000;
+        color: #fff;
+        border-radius: 10px;
+        position: absolute;
+        left: 110px;
+        bottom: 35px;
       }
-      .van-tab__pane {
-        margin: 0 auto;
-        width: 100%;
-        height: 100%;
-        background: #fff;
+      span:nth-child(2) {
+        text-align: center;
+        width: 40px;
+        height: 20px;
+        border-radius: 10px;
+        background-color: #fff;
+        position: absolute;
+        left: 180px;
+        bottom: 35px;
+        padding: 0 5px;
+        line-height: 20px;
       }
-    }
-    .musiclist {
-      width: 90%;
     }
   }
+  .van-icon {
+    font-size: 20px;
+    position: absolute;
+    top: 30px;
+    right: 30px;
+  }
+
+  .list {
+    height: 160px;
+    width: 90%;
+    background-color: #ffffff;
+    margin: 0 auto;
+    border-radius: 10px;
+    .list-inner {
+      height: 80px;
+      text-align: center;
+      #tubiao {
+        font-size: 36px;
+        margin-top: 10px;
+        color: red;
+      }
+      #tubiao8 {
+        font-size: 36px;
+        margin-top: 10px;
+        color: #a8a8a8;
+      }
+      p {
+        height: 20px;
+        line-height: 20px;
+        margin-top: 5px;
+        color: #5a5a5a;
+      }
+    }
+  }
+  .like {
+    height: 80px;
+    width: 90%;
+    margin: 10px auto;
+    background-color: #fff;
+    padding: 10px 10px;
+    position: relative;
+    border-radius: 10px;
+    .like-left {
+      height: 100%;
+      width: 60px;
+      background-color: #000;
+      opacity: 0.9;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 10px;
+      }
+    }
+    .like-main {
+      float: left;
+      p:nth-child(1) {
+        margin-left: 15px;
+        margin-top: 5px;
+        font-size: 16px;
+      }
+      p:nth-child(2) {
+        margin-left: 15px;
+        margin-top: 2px;
+        color: #8d8d8d;
+        font-size: 12px;
+      }
+    }
+    .like-right {
+      p {
+        width: 60px;
+        height: 13px;
+        border-radius: 14px;
+        padding: 5px 7px;
+        line-height: 13px;
+        border: 1px solid #8d8d8d;
+        position: absolute;
+        top: 27px;
+        right: 20px;
+        i {
+          height: 100%;
+        }
+        span {
+          height: 100%;
+        }
+      }
+    }
+  }
+  .van-tabs {
+    height: 50px;
+    width: 90%;
+    margin: 0 20px;
+    p {
+      font-size: 16px;
+      text-align: center;
+      line-height: 45px;
+      color: #000;
+    }
+    .van-tab__pane {
+      margin: 0 auto;
+      width: 100%;
+      height: 100%;
+      background: #fff;
+    }
+  }
+  .musiclist {
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    img {
+      width: 60px;
+      margin-left: 20px;
+    }
+  }
+}
 </style>
